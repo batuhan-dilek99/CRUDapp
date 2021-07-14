d250e8c2687dd093c31300114b09b05e7ef2506e
batuhan-dilek99 <80598580+batuhan-dilek99@users.noreply.github.com>
2021-07-14 16:55:16 +0300

    Delete src directory

--- src/controller/base_controller.ts
+++
@@ -1,8 +0,0 @@
-import * as express from "express";
-export default class BaseController {
-    router: express.Router;
-  
-    constructor() {
-      this.router = express.Router();
-    }
-  }
--- src/repository/game_repository.ts
+++
@@ -1,5 +0,0 @@
-import { games, Game } from "../db/game_interface";
-
-class GameRepo{
-    //TO BE COMPLETED WHEN KNEX IS INSTALLED 
-}
