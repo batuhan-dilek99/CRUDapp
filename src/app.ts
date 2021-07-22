23c8f457f1840bb3d50c7a169ea9b8c5c02aa81f
batuhan-dilek99 <80598580+batuhan-dilek99@users.noreply.github.com>
2021-07-22 13:39:54 +0300

    Updates
    resolve and reject has been added to init();

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import {GameController} from "./controller/game_controller";
+import gamecontroller from "./controller/game_controller";
 import errorHandler from "./middleware/errorMiddleWare";
-
+import db from "./db/knex";
 
 class GameApp{
     app : express.Application;
@@ -14,38 +14,45 @@ class GameApp{
     init(){
         return new Promise((resolve, reject) => {
             try{
-                this.app.appConfiguration();   //TBD
-                this.app.routeConfiguration(); //TBD
+                this.appConfiguration();   
+                this.routeConfiguration(); 
+                db.init();
             }
             catch(error){
                 console.log(error);
+                reject(error);
             }
             finally{
                 this.app.use(errorHandler);
+                console.log("App configured.");
+                resolve(true);
             }
         });
     }
 
     private appConfiguration(){
-        //KNEX WILL BE INITIALIZED HERE.
         this.app.use(express.json());
     }
 
     private routeConfiguration() {
         const apiPath : string = "/api"
         this.app.use(apiPath, this.route);
-        this.route.use("/", GameController);
+        this.route.use("/games", gamecontroller);
     }
     
     listen(): Promise<boolean> {
         return new Promise((resolve, reject) => {
-    
-          const port = Number(process.env.APP_PORT || 3000);
-          this.app.listen(port, () => {
-            console.log("Express server started on port: " + port);
-            resolve(true);
-          });
-    
+            const port = Number(process.env.APP_PORT || 3000);
+            try{
+                this.app.listen(port, () => {
+                    console.log("Express server started on port: " + port);
+                    resolve(true);
+                });
+            }
+            catch(error){
+                console.log("Cannot started on port : " + port);
+                reject(error);
+            }
         });
       }
 }
